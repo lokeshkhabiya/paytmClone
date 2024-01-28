1f6f23c39bd2324014baa7b2148299ffbf6d4ac1
lokeshkhabiya <lokeshkhabiya0011@gmail.com>
2024-01-28 10:58:08 +0530

    added transactions in database

--- backend/db.js
+++ backend/db.js
@@ -33,10 +33,24 @@ const userSchema = new mongoose.Schema({
     },
 })
 
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+})
+
 // create a model from the schema 
 
+const Account = mongoose.model('Account', accountSchema);
 const User = mongoose.model('User', userSchema);
 
 module.exports = {
-    User
+    User,
+    Account
 }
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,12 @@
+const express = require('express');
+const cors = require("cors");
+const rootRouter = require("./routes/index");
 
-const express = require("express");
-const cors = require('cors');
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const rootRouter = require('./routes/index');
-const app = express();
+app.use("/api/v1", rootRouter);
 
-
-
-app.use('/api/v1', rootRouter);
-app.listen(3000);
-
+app.listen(3003);
